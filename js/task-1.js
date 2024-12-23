b4593896b524d3b85a9bc8417b0f4c42c470e6e8
elen-purple <anna.havrylova.2011@gmail.com>
2024-12-23 13:27:01 +0200

    time 3

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,32 @@
 const gallery = document.querySelector(".gallery");
+const closeBtn = document.querySelector(`[data-action="close"]`);
+const openBtn = document.querySelector(`[data-action="open"]`);
+const backdrop = document.querySelector(`.full-image-container`);
+gallery.style.marginLeft = 0;
 window.addEventListener("keydown", (e) => {
-  if (e.code === "ArrowLeft") {
-    gallery.scrollLeft -= 320;
+  if (
+    e.code === "ArrowRight" &&
+    Number.parseInt(gallery.style.marginLeft) !== -2100
+  ) {
+    gallery.style.marginLeft = `${
+      Number.parseInt(gallery.style.marginLeft) -
+      (gallery.firstElementChild.offsetWidth + 20)
+    }px`;
   }
-  if (e.code === "ArrowRight") {
-    gallery.scrollLeft += 320;
+  if (
+    e.code === "ArrowLeft" &&
+    Number.parseInt(gallery.style.marginLeft) !== 0
+  ) {
+    gallery.style.marginLeft = `${
+      Number.parseInt(gallery.style.marginLeft) +
+      (gallery.firstElementChild.offsetWidth + 20)
+    }px`;
   }
 });
+
+openBtn.addEventListener("click", toggleModal);
+closeBtn.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  backdrop.classList.toggle("is-hidden");
+}
--- js/task-2.js
+++ js/task-2.js
@@ -3,22 +3,24 @@ const renderBtn = document.querySelector(`[data-action="render"]`);
 const destroyBtn = document.querySelector(`[data-action="destroy"]`);
 const boxes = document.querySelector("#boxes");
 
+let size = 30;
+let color = "";
 const createBoxes = (amount) => {
   for (let i = 0; i < amount; i += 1) {
+    color = `rgb(${Math.round(Math.random() * (255 - 0) + 0)}, ${Math.round(
+      Math.random() * (255 - 0) + 0
+    )}, ${Math.round(Math.random() * (255 - 0) + 0)})`;
     boxes.insertAdjacentHTML(
       "beforeend",
-      `<div  style=" min-width: 100px; height: 100px;background-color: rgb(${Math.round(
-        Math.random() * (255 - 0) + 0
-      )}, ${Math.round(Math.random() * (255 - 0) + 0)}, ${Math.round(
-        Math.random() * (255 - 0) + 0
-      )})"></div>`
+      `<div style=" width: ${size}px; height: ${size}px; background-color: ${color}"></div>`
     );
-    console.log(boxes);
+    size += 10;
   }
 };
 
 const destroyBoxes = () => {
   boxes.innerHTML = "";
+  size = 30;
 };
 
 renderBtn.addEventListener("click", () => {
